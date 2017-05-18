b3ae6b167d3c5bb54f721e221486d1e586e821ad
Ali Abbas <ali5ch@hotmail.com>
2017-05-18 22:21:37 +0200

    K-Means Clustering in R

---
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/K-Means_Clustring.R
@@ -0,0 +1,33 @@
+# K-means Clusttring 
+
+# Import the dataset
+dataset <- read.csv("Mall_Customers.csv")
+X <- dataset[4:5]
+
+#Using the Elbow method to find the optional number of clusters
+set.seed(6)
+wcss <- vector()
+for(i in 1:10) wcss[i] <- sum(kmeans(X,i)$withinss)
+plot(1:10,wcss, type = "b", main = paste('Clusters of clients'), xlab= "Number of clusters", ylab = "WCSS")
+
+#Aplying k-means to the mall dataset
+set.seed(29)
+kmeans <- kmeans(X,5,iter.max = 300, nstart = 10)
+
+#Visualising the cluster 
+library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Cluster of clients'),
+         xlab= "Annual Income",
+         ylab = "Spending Score")
+
+
+
+
